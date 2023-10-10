b7fc8037e91af00f3a17bffea4fb2d9d23c79e31
Maksym Cherniienko <h0nza4ok@gmail.com>
2023-10-10 13:34:28 +0300

    1

--- Lab_04_4/Lab_04_4.cpp
+++ Lab_04_4/Lab_04_4.cpp
@@ -12,7 +12,7 @@ using namespace std;
 
 int main()
 {
-	double F, R, x, xk, dx;
+	double y, R, x, xk, dx;
 	cout << "xp = "; cin >> x;
 	cout << "xk = "; cin >> xk;
 	cout << "dx = "; cin >> dx;
@@ -21,22 +21,22 @@ int main()
 	cout << fixed;
 	cout << "---------------------------" << endl;
 	cout << "|" << setw(5) << "x" << "     |"
-		<< setw(7) << "F" << "      |" << endl;
+		<< setw(7) << "y" << "      |" << endl;
 	cout << "---------------------------" << endl;
 	for (x; x <= xk; x += dx)
 	{
 		if (x <= -2)
-			F = -1 + (x + 4);
+			y = -1 + (x + 4);
 		else
 			if (x > -2 && x <= 4)
-				F = 1 + (-R - 1) / 6 * (x + 2);
+				y = 1 + (-R - 1) / 6 * (x + 2);
 			else
 				if (x > 8 - R && x <= 8 + R)
-					F = sqrt(pow(R, 2) - pow(x, 2) + 16 * x - 64) - R;
+					y = sqrt(pow(R, 2) - pow(x, 2) + 16 * x - 64) - R;
 				else
-					F = -R;
+					y = -R;
 		cout << "|" << setw(7) << setprecision(2) << x
-			<< "   |" << setw(10) << setprecision(3) << F
+			<< "   |" << setw(10) << setprecision(3) << y
 			<< "   |" << endl;
 	}
 }
